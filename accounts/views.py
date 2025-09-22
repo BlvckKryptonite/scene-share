ab27f50a7cce229911038f7008fbe48831304981
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-22 18:56:34 +0200

    Create registration view. If valid, a success message is shown and we redirect to login.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+
+# I am defining the registration page view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # This creates a new user in the database
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
