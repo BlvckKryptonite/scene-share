2c0db0f9f27ba1be45b4f3575a833bc958223f53
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-23 20:23:01 +0200

    Edited movies view to fetch movies added from database to display on frontend.

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    return render(request, 'movies/home.html')
+    movies = Movie.objects.all()
+    return render(request, 'movies/home.html', {'movies': movies})
+
