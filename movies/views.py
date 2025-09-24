ffc9cbc1eb1ef8df57d0903c9859ce4a41eb89fa
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-24 12:58:34 +0200

    Update homepage view to handle both displaying existing reviews and processing new review submissions.

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Review
+from .forms import ReviewForm
 
 # Create your views here.
 def home(request):
     movies = Movie.objects.all()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.movie_id = request.POST.get('movie_id')  # hidden input in form
+            review.save()
+            return redirect('home')
+    else:
+        form = ReviewForm()
+
+    context = {
+        'movies': movies,
+        'form': form,
+    }
+
     return render(request, 'movies/home.html', {'movies': movies})
-
-
-
-
