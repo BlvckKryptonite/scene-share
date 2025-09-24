be25133277fca4ae04b83aa940a63810d07bf9cb
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-24 11:45:29 +0200

    Created movie review model and registered it to admin.

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 # Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'release_year')
     search_fields = ('title',)
+
+# Reviews
+admin.site.register(Review)
