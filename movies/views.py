a8a53a4a4c05e28334690fb3a64f7e58da1a361d
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-25 21:22:22 +0200

    Improve movie card UI to excplicitly show edit/delete option for user reviews.

--- movies/views.py
+++ movies/views.py
@@ -1,39 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Avg
 from .models import Movie, Review
 from .forms import ReviewForm
 
-# Create your views here.
 def home(request):
-    movies = Movie.objects.all()
+    # Annotate movies with their average rating
+    movies = Movie.objects.all().annotate(avg_rating=Avg('reviews__rating'))
 
     # Initialize a dict of forms for all movies
     forms_dict = {movie.id: ReviewForm() for movie in movies}
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
-        
+        movie_id = request.POST.get('movie_id')
+        movie = get_object_or_404(Movie, pk=movie_id)
+
         if form.is_valid():
-            movie_id = request.POST.get('movie_id')
-            movie = Movie.objects.get(pk=movie_id)
-            
-            # Users can create or update reviews
-            review, created = Review.objects.update_or_create(
+            # Create or update review for this user/movie
+            Review.objects.update_or_create(
                 user=request.user,
                 movie=movie,
                 defaults={
                     'rating': form.cleaned_data['rating'],
                     'comment': form.cleaned_data['comment']
-    }
+                }
             )
             return redirect('home')
         else:
-            # Replace the form for the movie that failed validation
-            movie_id = int(request.POST.get('movie_id'))
-            forms_dict[movie_id] = form
+            # Replace the form in forms_dict with the invalid one
+            forms_dict[int(movie_id)] = form
 
     context = {
         'movies': movies,
-        'forms_dict': forms_dict,  # Always pass the dict to template
+        'forms_dict': forms_dict
     }
 
     return render(request, 'movies/home.html', context)
