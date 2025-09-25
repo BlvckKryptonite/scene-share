d8c78a573b4532af8644837f4645953fc594ac06
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-25 21:24:43 +0200

    Defined edit/delete urls in movie/urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
