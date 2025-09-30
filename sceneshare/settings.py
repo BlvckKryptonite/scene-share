e7294231fa3445f1173747cfb0d949c1a40e46bb
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-09-30 11:52:42 +0200

    Concealed scecret key and API key.

--- sceneshare/settings.py
+++ sceneshare/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2^$8nl0=$+6v$*oz_cjtj6w8b57gu(=fs2x2osj*mn0j#4i@s'
+# Concealed SECRET_KEY
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -145,3 +146,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'  # Redirect after logout
 # - CLOUDINARY OR AWS S3
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# API KEY
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
