f4c5d438f9eb08b742ed9e73ebcd68feef5aa50f
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-10-02 16:55:02 +0200

    Updated admin.py to  display actual  movie posters as thumbnails(instead of links) and unify release year. This is so I can easily verify that each displayed movie poster is accurate. Also easier to update if not.

--- movies/admin.py
+++ movies/admin.py
@@ -1,24 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Movie, Review
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ("title", "get_release_year", "get_poster")
+    list_display = ("title", "get_release_year", "get_poster_thumb")
     search_fields = ("title",)
 
     def get_release_year(self, obj):
-        if obj.release_date:
-            return obj.release_date.split("-")[0]  # Extract year from YYYY-MM-DD
+        if obj.release_year:
+            return obj.release_year
+        elif obj.release_date:
+            return obj.release_date[:4]  # extract YYYY
         return None
     get_release_year.short_description = "Release Year"
 
-    def get_poster(self, obj):
-        if obj.poster_path:
-            return f"https://image.tmdb.org/t/p/w200{obj.poster_path}"
-        return None
-    get_poster.short_description = "Poster"
+    def get_poster_thumb(self, obj):
+        if obj.poster:
+            return format_html('<img src="{}" width="60" />', obj.poster.url)
+        elif obj.poster_path:
+            return format_html('<img src="https://image.tmdb.org/t/p/w200{}" width="60" />', obj.poster_path)
+        return "No Poster"
+    get_poster_thumb.short_description = "Poster"
 
-# Reviews
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("movie", "user", "rating", "created_at")
