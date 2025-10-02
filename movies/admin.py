6a0f15b721ca77e6ac156a3eb1ef6bb7226cc1d6
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-10-02 16:51:28 +0200

    Updated home.html  so that both manually uploaded movies and API-fetched movies show the correct poster and release year.

--- movies/admin.py
+++ movies/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from .models import Movie, Review
 
-# Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_year')
-    search_fields = ('title',)
+    list_display = ("title", "get_release_year", "get_poster")
+    search_fields = ("title",)
+
+    def get_release_year(self, obj):
+        if obj.release_date:
+            return obj.release_date.split("-")[0]  # Extract year from YYYY-MM-DD
+        return None
+    get_release_year.short_description = "Release Year"
+
+    def get_poster(self, obj):
+        if obj.poster_path:
+            return f"https://image.tmdb.org/t/p/w200{obj.poster_path}"
+        return None
+    get_poster.short_description = "Poster"
 
 # Reviews
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("movie", "user", "rating", "created_at")
+    search_fields = ("movie__title", "user__username")
