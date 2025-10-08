b5aee872a4288edee82fb8e155df88a5c80332ce
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-10-08 13:14:48 +0200

    Created url.py file in community app. Added review like view to it. Updated main url.py file with community app url.

---
+++ community/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("like/<int:review_id>/", views.toggle_review_like, name="toggle_review_like"),
+]
--- sceneshare/urls.py
+++ sceneshare/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('movies.urls')), # Home page points to movies app
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path("community/", include("community.urls")),
 ]
 
 # Will allow my development server to serve avatars for testing
