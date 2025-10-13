557732675987e0dbc92d40d80f10c555c77c23aa
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-10-13 10:55:13 +0200

    Removed comment class from community model. Can be implemented later as a future enhancement.

--- community/models.py
+++ community/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from movies.models import Review 
-from django.utils import timezone # For review tiem posted.
 
 # Create your models here.
+
+
 class ReviewLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="reviewlike_set")
@@ -14,13 +15,3 @@ class ReviewLike(models.Model):
 
     def __str__(self):
         return f"{self.user.username} liked {self.review.id}"
-
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField(max_length=500)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Comment by {self.user.username} on {self.review}"
--- sceneshare/settings.py
+++ sceneshare/settings.py
@@ -34,7 +34,7 @@ if not SECRET_KEY:
     raise ValueError("SECRET_KEY not set in environment!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sceneshare-0073094647bb.herokuapp.com', 'localhost', '127.0.0.1']
 
